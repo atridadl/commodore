cafecc9026c79c16671cc30661dccba7bfe52b52
atridadl <me@atri.dad>
2023-11-13 17:37:44 -0700

    Env Changes

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,28 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <SignIn
+      appearance={{
+        elements: {
+          socialButtonsBlockButton__google: {
+            display: "none",
+          },
+          socialButtonsIconButton__google: {
+            display: "none",
+          },
+          socialButtonsProviderIcon_google: {
+            display: "none",
+          },
+          socialButtonsBlockButton__github: {
+            display: "none",
+          },
+          socialButtonsIconButton__github: {
+            display: "none",
+          },
+          socialButtonsProviderIcon_github: {
+            display: "none",
+          },
+        },
+      }}
       path="/sign-in"
       routing="path"
       signUpUrl="/sign-up"
